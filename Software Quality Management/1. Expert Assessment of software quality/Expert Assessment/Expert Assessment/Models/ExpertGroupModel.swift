2f1ac275bd7666e54d6fccdb4a4673a65ef9835d
verofieiev <ext-vladyslav.yerofieiev@here.com>
2018-10-12 18:51:41 +0300

    Initial commit

---
+++ Software Quality Management/1. Expert Assessment of software quality/Expert Assessment/Expert Assessment/Models/ExpertGroupModel.swift
@@ -0,0 +1,131 @@
+//
+//  ExpertGroupModel.swift
+//  Expert Assessment
+//
+//  Created by verofieiev on 10/12/18.
+//  Copyright © 2018 Vladyslav Yerofieiev. All rights reserved.
+//
+
+import Foundation
+
+struct ExpertGroupTable {
+    var experts: [ExpertAppraisal] = []
+    var name: String = ""
+    
+    
+    /// Сумарна Точність управління та обчислень
+    var summAccuracyOfControlAndComputing: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.accuracyOfControlAndComputing
+        })
+    }
+    /// Сумарна Ступінь стандартності інтерфейсів
+    var summDegreeOfStandardInterfaces: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.degreeOfStandardInterfaces
+        })
+    }
+    /// Сумарна Функціональна повнота
+    var summFunctionalCompleteness: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.functionalCompleteness
+        })
+    }
+    /// Сумарна Стійкість до помилок
+    var summErrorsResilience: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.errorsResilience
+        })
+    }
+    /// Сумарна Можливість розширення
+    var summExpandAbility: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.expandAbility
+        })
+    }
+    /// Сумарна Зручність роботи
+    var summWorkConvenience: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.workConvenience
+        })
+    }
+    /// Сумарна Простота роботи
+    var summWorkSimplicity: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.workSimplicity
+        })
+    }
+    /// Сумарна Відповідність чинним стандартам
+    var summStandardsCompliance: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.standardsCompliance
+        })
+    }
+    /// Сумарна Переносимість між ПЗ
+    var summSoftwarePortability: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.softwarePortability
+        })
+    }
+    /// Сумарна Зручність навчання
+    var summStudyConvenience: Float {
+        return experts.reduce(0.0, { (current, expert) -> Float in
+            return current+expert.studyConvenience
+        })
+    }
+    
+    /// Середня Точність управління та обчислень
+    var averageAccuracyOfControlAndComputing: Float {
+        return summAccuracyOfControlAndComputing / Float(experts.count)
+    }
+    /// Середня Ступінь стандартності інтерфейсів
+    var averageDegreeOfStandardInterfaces: Float {
+        return summDegreeOfStandardInterfaces / Float(experts.count)
+    }
+    /// Середня Функціональна повнота
+    var averageFunctionalCompleteness: Float {
+        return summFunctionalCompleteness / Float(experts.count)
+    }
+    /// Середня Стійкість до помилок
+    var averageErrorsResilience: Float {
+        return summErrorsResilience / Float(experts.count)
+    }
+    /// Середня Можливість розширення
+    var averageExpandAbility: Float {
+        return summExpandAbility / Float(experts.count)
+    }
+    /// Середня Зручність роботи
+    var averageWorkConvenience: Float {
+        return summWorkConvenience / Float(experts.count)
+    }
+    /// Середня Простота роботи
+    var averageWorkSimplicity: Float {
+        return summWorkSimplicity / Float(experts.count)
+    }
+    /// Середня Відповідність чинним стандартам
+    var averageStandardsCompliance: Float {
+        return Float(summStandardsCompliance) / Float(experts.count)
+    }
+    /// Середня Переносимість між ПЗ
+    var averageSoftwarePortability: Float {
+        return summSoftwarePortability / Float(experts.count)
+    }
+    /// Середня Зручність навчання
+    var averageStudyConvenience: Float {
+        return summStudyConvenience / Float(experts.count)
+    }
+    
+    
+    func toExpertAppraisal() -> ExpertAppraisal {
+        return ExpertAppraisal(accuracyOfControlAndComputing: averageAccuracyOfControlAndComputing,
+                               degreeOfStandardInterfaces: averageDegreeOfStandardInterfaces,
+                               functionalCompleteness: averageFunctionalCompleteness,
+                               errorsResilience: averageErrorsResilience,
+                               expandAbility: averageExpandAbility,
+                               workConvenience: averageWorkConvenience,
+                               workSimplicity: averageWorkSimplicity,
+                               standardsCompliance: averageStandardsCompliance,
+                               softwarePortability: averageSoftwarePortability,
+                               studyConvenience: averageStudyConvenience)
+    }
+}
